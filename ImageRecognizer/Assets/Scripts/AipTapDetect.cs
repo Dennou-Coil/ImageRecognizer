6114a9d526e4e7f96755283a35e824433de93e67
Daichi Ito <math144377@gmail.com>
2018-03-21 17:24:50 +0900

    Add AirTapDetect to change texts

---
+++ ImageRecognizer/Assets/Scripts/AipTapDetect.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine.UI;
+
+public class AipTapDetect : MonoBehaviour, IInputClickHandler {
+    public string first;
+    public string second;
+
+    [SerializeField] private bool isFirst;
+    private Text textData;
+
+
+    // Use this for initialization
+    void Start () {
+        isFirst = true;
+        textData = gameObject.GetComponentInChildren<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        isFirst = !isFirst;
+        textData.text = (isFirst) ? first : second;
+    }
+}
