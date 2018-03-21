7bd7e165678fb9a474bf59f88fb1ee047c7d47fd
Daichi Ito <math144377@gmail.com>
2018-03-21 16:46:51 +0900

    Add AddColliderToUIPanel.cs to attach collider automatically

---
+++ ImageRecognizer/Assets/Scripts/AddColliderToUIPanel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddColliderToUIPanel : MonoBehaviour
+{
+    void Start()
+    {
+        BoxCollider box = gameObject.AddComponent<BoxCollider>();
+        Vector2 objSize = GetComponent<RectTransform>().sizeDelta;
+        box.size = new Vector3(objSize.x,objSize.y,0.1f);
+    }
+}
